b281763cab93872377ca2ae330939736be75012d
Frantz Dorismond <frantz.n.dorismond@gmail.com>
2019-04-29 21:44:07 +0000

    latest

--- js/game.js
+++ js/game.js
@@ -6,40 +6,88 @@ var game = {
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "You walk on a street",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Go Left",
+                    nextLevel: "sewer",
                 },
 
                 {
-                    text: "Keep on moving",
-                    nextLevel: "field",
+                    text: "Go Right",
+                    nextLevel: "hundred",
                 },
             ]
         },
 
-        cave: {
+        hundred: {
             background_image: "fire.gif",
             music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+            message: "You pick $100 off of the ground and walk a bit more ",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "Keep it",
+                    nextLevel: "trick",
+                },
+                {
+                    text:"Give it to the homeless",
+                    nextLevel:"honorable",
                 },
             ]
         },
 
-        field: {
-            message: "Some adventurer you are...",
+        sewer: {
+            message: "You walk straight into an open sewer.",
             choices: [
                 {
-                    text: "Start over",
+                    text: "Try Again",
                     nextLevel: "start",
                 },
             ]
+        },
+        
+        trick: {
+            message:"April Fools! The bill turns into a paint bomb and leaves you drenched.",
+            choices: [
+                {
+                    text:"Try Again",
+                    nextLevel:"start",
+                },
+                ]
+        },
+        
+        honorable: {
+            message:"You have done a great deed. Congratulations!",
+            choices:[
+                {
+                    text:"Take the train",
+                    nextLevel:"Winner",
+                },
+                {
+                    text:"Pick up Uber",
+                    nextLevel:"Rip",
+                },
+                ]
+        },
+        
+        Rip: {
+            message:"Water on the street drenches you as the Uber comes.",
+            choices:[
+                {
+                    text:"Try Again",
+                    nextLevel:"start",
+                },
+                ]
+        },
+        
+        Winner: {
+            message:"You win, you reach your destination on time.",
+            choices:[
+                {
+                    text:"Thank you for playing!",
+                    nextLevel:"start",
+                },
+                ]
         },
 
     }
